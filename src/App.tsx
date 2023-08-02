b8e374b319911fc169b960ab6b94c3c17b94f1eb
salhi50 <nonosfafa@gmail.com>
2023-08-03 00:51:12 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+import { initializer, todoReducer, Actions } from "./todoReducer";
+
+export interface Todo {
+	title: string;
+	done: boolean;
+	id: string | number;
+	isRoot: boolean;
+}
+
+export type AddTodoFunction = (index: number) => void;
+export type UpdateTodoTitleFunction = (id: Todo["id"], title: Todo["title"]) => void;
+export type DeleteTodoFunction = (id: Todo["id"]) => void;
+export type ToggleTodoCompleteFunction = (id: Todo["id"], done: Todo["done"]) => void;
+
+const App: React.FC = () => {
+	const [todos, dispatch] = React.useReducer(todoReducer, undefined, initializer);
+
+	const addTodo: AddTodoFunction = (lastTodoItemIndex) => {
+		dispatch({
+			type: Actions.ADD,
+			payload: {
+				index: lastTodoItemIndex,
+			},
+		});
+	};
+
+	const updateTodoTitle: UpdateTodoTitleFunction = (id, title) => {
+		dispatch({
+			type: Actions.UPDATE_TITLE,
+			payload: { id, title },
+		});
+	};
+
+	const deleteTodo: DeleteTodoFunction = (id) => {
+		dispatch({
+			type: Actions.DELETE,
+			payload: { id },
+		});
+	};
+
+	const toggleTodoComplete: ToggleTodoCompleteFunction = (id, done) => {
+		dispatch({
+			type: Actions.TOGGLE_COMPLETE,
+			payload: { id, done },
+		});
+	};
+
+	return (
+		<>
+			<div
+				className="container mx-auto py-5"
+				style={{ maxWidth: 500 }}
+			>
+				<ul className="list-unstyled vstack gap-3">
+					{todos.map((todo, index) => (
+						<li key={todo.id}>
+							<TodoItem
+								todo={todo}
+								index={index}
+								addTodo={addTodo}
+								updateTodoTitle={updateTodoTitle}
+								deleteTodo={deleteTodo}
+								toggleTodoComplete={toggleTodoComplete}
+							/>
+						</li>
+					))}
+				</ul>
+			</div>
+		</>
+	);
+};
+
+export default App;
---
+++ src/TodoItem.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import {
+	AddTodoFunction,
+	DeleteTodoFunction,
+	Todo,
+	ToggleTodoCompleteFunction,
+	UpdateTodoTitleFunction,
+} from "./App";
+
+interface TodoItemProps {
+	todo: Todo;
+	index: number;
+	addTodo: AddTodoFunction;
+	updateTodoTitle: UpdateTodoTitleFunction;
+	deleteTodo: DeleteTodoFunction;
+	toggleTodoComplete: ToggleTodoCompleteFunction;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({
+	todo,
+	addTodo,
+	index,
+	updateTodoTitle,
+	deleteTodo,
+	toggleTodoComplete,
+}) => {
+	React.useEffect(() => {
+		return () => {
+			const prevTodoInput = document.querySelector(
+				`input[data-index="${index - 1}"]`,
+			) as HTMLInputElement;
+			if (prevTodoInput) setTimeout(() => prevTodoInput.focus());
+		};
+	}, []);
+
+	const handleKeydown = (e: React.KeyboardEvent) => {
+		const isEmpty = todo.title.length === 0;
+		switch (e.key) {
+			case "Enter":
+				if (!isEmpty) addTodo(index);
+				break;
+			case "Backspace":
+				if (isEmpty && !todo.isRoot) deleteTodo(todo.id);
+				break;
+		}
+	};
+
+	return (
+		<>
+			<div className="hstack gap-3 flex-nowrap align-items-center bg-light p-3 border">
+				<input
+					type="checkbox"
+					className="form-check-input flex-shrink-0 form-check-input-lg"
+					style={{ width: "1.5em", height: "1.5em" }}
+					checked={todo.done}
+					onChange={(e) => toggleTodoComplete(todo.id, e.target.checked)}
+				/>
+				<input
+					type="text"
+					className="border-0 border-bottom form-control form-control-sm flex-grow-1 rounded-0 shadow-none"
+					placeholder="To-do"
+					data-index={index}
+					value={todo.title}
+					autoFocus
+					onChange={(e) => updateTodoTitle(todo.id, e.target.value)}
+					onKeyDown={handleKeydown}
+					style={{ textDecoration: todo.done ? "line-through" : "none" }}
+				/>
+				{!todo.isRoot && (
+					<button
+						className="flex-shrink-0 btn btn-light d-flex align-items-center justify-content-center"
+						style={{ width: "2em", height: "2em" }}
+						type="button"
+						onClick={() => deleteTodo(todo.id)}
+					>
+						&times;
+					</button>
+				)}
+			</div>
+		</>
+	);
+};
+
+export default TodoItem;
